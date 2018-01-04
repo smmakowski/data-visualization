5aa0b5831051b5d35803204cc5c5f1d6ec18a457
smmakowski <makowski.sm@gmail.com>
2018-01-03 17:26:53 -0800

    retrieve JSON data from web using promises

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 $(document).ready(() => {
-  console.log('ready');
-})
+  const urls = {
+    barChart: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
+    scatterPlot: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json',
+    heatMap: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json',
+    forceDirectedGraph: 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json',
+    meteoriteMap: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json'
+  }
+
+  Promise.all([
+    $.getJSON(urls['barChart']),
+    $.getJSON(urls['scatterPlot']),
+    $.getJSON(urls['heatMap']),
+    $.getJSON(urls['forceDirectedGraph']),
+    $.getJSON(urls['meteoriteMap']),
+  ])
+  .then((allData) => {
+    console.log(allData);
+  });
+});
